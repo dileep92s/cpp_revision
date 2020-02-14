2df193bb8d7c2d69acc4623ec9e828f6a58aceca
dileep92s <dileep92s@gmail.com>
2020-02-14 20:48:51 +0530

    build scripy

--- build.sh
+++ build.sh
@@ -5,14 +5,17 @@ app=${1}
 echo --------------------------
 echo make ${app} 
 echo --------------------------
+echo 
 make ${app} 
-
+echo 
 
 if [ $? = 0 ]
 then
     echo --------------------------
     echo ./bin/${app:-hello_world}
     echo --------------------------
+    echo 
     ./bin/${app:-hello_world}
+    echo 
 
 fi 
